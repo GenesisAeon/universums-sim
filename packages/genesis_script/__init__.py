72b1dbed97e639e49d1e855f4f0020dd6a44a8af
GenesisAeon <joberoemer@gmail.com>
2025-06-10 08:57:02 +0200

    feat: add sync manifest and extend engine

--- packages/genesis_script/__init__.py
+++ packages/genesis_script/__init__.py
@@ -5,6 +5,11 @@ class Engine:
         self.entities = []
         self.tick = 0
 
+    def add_entity(self, entity):
+        """Fügt eine Entität zur Simulation hinzu."""
+        if entity is not None:
+            self.entities.append(entity)
+
     def step(self):
         """Führt einen Simulationsschritt aus."""
         self.tick += 1
